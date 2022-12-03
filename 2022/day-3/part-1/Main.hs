2ea74d475bc0c5daf42ff0f63eb81d90eb183306
Nicolas Reyland <nicolas@reyland.dev>
2022-12-03 15:53:24 +0100

    day-3: using uncurry function where appropriate

--- 2022/day-3/part-1/Main.hs
+++ 2022/day-3/part-1/Main.hs
@@ -4,9 +4,7 @@ import Data.List (intersect)
 import Data.Char (ord, isLower)
 
 findItem  :: String -> Char
-findItem s = head $ intersect s1 s2
-    where
-        (s1, s2) = splitAt ((length s + 1) `div` 2) s
+findItem s = head $ uncurry intersect $ splitAt ((length s + 1) `div` 2) s
 
 charPriority :: Char -> Int
 charPriority c
